92e455edc5e41f2a998201a504f00afe97d47402
Baeyonghyeon <qodydgus97@naver.com>
2021-07-11 16:59:13 +0900

    완성

---
+++ src/baek_2231.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class baek_2231 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int total, result=0;
+
+        for(int i=0; i<N; i++){
+
+            int x= i;
+            total = i;
+
+            while(true){
+                total += x%10; //자리수 더하기
+                x /= 10;
+
+                if(x == 0) break;
+            }
+            if(total == N){
+                result=i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/baek_2798.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class baek_2798 {
+
+    static int x, y, z, total = 0;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt(); //카드수
+        int M = scan.nextInt(); //3장 카드합
+        int card[] = new int[N]; //카드입력
+        for (int i = 0; i < N; i++) {
+            card[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            x = card[i];
+            for (int o= i+1; o < N - 1; o++) {
+                y = card[o];
+                for (int p= o+1 ; p < N; p++) {
+                    z = card[p];
+                    if (x + y + z > total && x + y + z <= M) {
+                        total = x + y + z;
+                    }
+                }
+            }
+        }
+        System.out.println(total);
+    }
+}
