d01712729d2e67d37dcfbd13ce09c8bbeed984cc
Baeyonghyeon <qodydgus97@naver.com>
2021-09-02 23:30:10 +0900

    통계학

--- src/baek/baek_2108.java
+++ src/baek/baek_2108.java
@@ -8,29 +8,39 @@ import java.util.Arrays;
 public class baek_2108 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int mean, median, mode, distance; //산술평균, 중앙값, 최빈값, 범위
+        int mean;
+        int median, mode, distance; //산술평균, 중앙값, 최빈값, 범위
         int N = Integer.parseInt(br.readLine());
         int list[] = new int[N];
-
+        int countlist[] = new int[8001]; // countlist[0 ~ 4001] : -1 ~ -4000 , countlist[4002 ~ 8001] : 0 ~ 4000
 
         for (int i = 0; i < N; i++) {
-            list[i] = Integer.parseInt(br.readLine());
+            int input = Integer.parseInt(br.readLine());
+            list[i] = input;
+            //최빈값
+            countlist[input+4000]++;
         }
 
         Arrays.sort(list);
 
-        mean = meancalculate(list);
-        median = mediancalculate(list);
-        mode = modecalculate(list);
-        distance = distancecalculate(list);
+        StringBuilder sb = new StringBuilder();
+
+        mean = meancalculate(list);         // 산술평균
+        median = mediancalculate(list);     // 중앙값
+        mode = modecalculate(countlist);    // 최빈값
+        distance = distancecalculate(list); // 범위
+
+        sb.append(mean).append('\n').append(median).append('\n').append(mode).append('\n').append(distance);
+        System.out.println(sb);
     }
 
     public static int meancalculate(int list[]) {
-        int value = 0;
+        double value = 0;
         for (int loop : list){
-            value =+ loop;
+            value += loop;
         }
-        return value/list.length;
+        value /= list.length;
+        return (int) Math.round(value);
     }
 
     public static int mediancalculate(int list[]){
@@ -38,8 +48,23 @@ public class baek_2108 {
         return value;
     }
 
-    public static int modecalculate(int list[]){ //이게 ㅁ어렵넴
-        return 0;
+    public static int modecalculate(int list[]){
+        int modevalue = 0;
+        boolean stack = false;
+        int firstchk = list[0];
+
+        for (int i=1; i<8001; i++){ // -4000부터 검사
+            if(list[i] > firstchk) { // 최빈값 가장 높은 값이 처음 걸릴때
+                firstchk = list[i];
+                modevalue = i;
+                stack = true;
+            } else if (list[i] == firstchk && stack == true) { // 현재 최빈값이랑 같은수의 최빈값이 또 걸릴때
+                firstchk = list[i];
+                modevalue = i;
+                stack = false;
+            }
+        }
+        return modevalue-4000;
     }
 
     public static int distancecalculate(int list[]){
