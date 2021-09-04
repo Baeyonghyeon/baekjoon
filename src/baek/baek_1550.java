ab755115970550c94f36df0a293c6696dc24684e
Baeyonghyeon <qodydgus97@naver.com>
2021-09-04 20:13:44 +0900

    16진수

---
+++ src/baek/baek_1550.java
@@ -0,0 +1,13 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baek_1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine(),16); // 16진수로 표현
+        System.out.println(N);
+    }
+}
