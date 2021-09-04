260345be0c3580541ce80371fae12f2853c25758
Baeyonghyeon <qodydgus97@naver.com>
2021-09-04 20:19:42 +0900

    긴자리 계산

--- src/baek/baek_2338.java
+++ src/baek/baek_2338.java
@@ -3,12 +3,16 @@ package baek;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class baek_2338 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String token[] = br.readLine().split(" ");
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());;
 
-
+        StringBuilder sb = new StringBuilder();
+        sb.append(A.add(B)).append('\n').append(A.subtract(B)).append('\n').append(A.multiply(B));
+        System.out.println(sb);
     }
 }
