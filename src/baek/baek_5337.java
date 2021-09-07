1d098d16541df84864b16d06f90b486e39d8e593
yonghyeon <qodydgus97@naver.com>
2021-09-07 22:40:46 +0900

    웰컴

---
+++ src/baek/baek_5337.java
@@ -0,0 +1,7 @@
+package baek;
+
+public class baek_5337 {
+        public static void main(String[] args) {
+            System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+        }
+}
