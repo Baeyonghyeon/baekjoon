7cd89784fc49d38e32345404588e1b8475a11b83
yonghyeon <qodydgus97@naver.com>
2021-09-13 19:40:50 +0900

    홍익대학교

---
+++ src/baek/baek_16394.java
@@ -0,0 +1,14 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(N - 1946);
+
+    }
+}
