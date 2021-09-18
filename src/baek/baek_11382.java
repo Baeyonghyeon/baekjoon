a4416c71e16d1ce011445c9ae174de2665ddff94
yonghyeon <qodydgus97@naver.com>
2021-09-18 22:31:29 +0900

    꼬마 정민

---
+++ src/baek/baek_11382.java
@@ -0,0 +1,24 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek_11382 {
+    public static void main(String[] args) {
+            long[] a = new long[3];
+            long sum = 0;
+
+            Scanner scanner = new Scanner(System.in);
+
+            for(int i = 0; i < 3; i++) {
+                a[i] = scanner.nextLong();
+            }
+
+            scanner.close();
+
+            for(int i = 0; i < 3; i++) {
+                sum = sum + a[i];
+            }
+
+            System.out.println(sum);
+    }
+}
