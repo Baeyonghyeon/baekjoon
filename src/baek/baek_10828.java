144d5ab4d5f6cdfe92af9ff1c3d8bf339ed55864
Baeyonghyeon <qodydgus97@naver.com>
2021-09-27 15:28:53 +0900

    stack 구성

--- src/baek/baek_10828.java
+++ src/baek/baek_10828.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class baek_10828 {
 
-    public static ArrayList<Integer> stack;
+    public static ArrayList<Integer> stack = new ArrayList<>();
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -23,16 +23,16 @@ public class baek_10828 {
                 push(Integer.parseInt(token[1]));
             }
             else if (token[0].equals("pop")){
-                sb.append(pop());
+                sb.append(pop()).append("\n");
             }
             else if (token[0].equals("size")){
-                sb.append(size());
+                sb.append(size()).append("\n");
             }
             else if (token[0].equals("empty")){
-                sb.append(empty());
+                sb.append(empty()).append("\n");
             }
             else if (token[0].equals("top")){
-                sb.append(top());
+                sb.append(top()).append("\n");
             }
         }
 
@@ -46,9 +46,14 @@ public class baek_10828 {
 
     // 스택에서 가장 위에 있는정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
     public static int pop(){
+        if(stack.size() == 0){
+            return -1;
+        }
+        else{
         int value = stack.get(stack.size()-1);
         stack.remove(stack.size()-1);
         return value;
+        }
     }
 
     // 스택에 들어있는 정수의 개수를 출력한다.
@@ -67,10 +72,10 @@ public class baek_10828 {
 
     // 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
     public static int top(){
-        int value = stack.get(stack.size()-1);
         if (stack.size() == 0){
             return -1;
         }else {
+            int value = stack.get(stack.size()-1);
             return value;
         }
     }
