de2ee08d38ecaf21c25b9c3ebaa7b7f5e468a260
yonghyeon <qodydgus97@naver.com>
2021-10-07 00:00:39 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ src/baek/baek_18108.java
@@ -0,0 +1,12 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N=sc.nextInt();
+        System.out.println(N-543);
+    }
+}
