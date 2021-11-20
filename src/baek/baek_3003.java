b3048d3822e68d7f718689b8fa38f6e9d494ed69
yonghyeon <qodydgus97@naver.com>
2021-11-20 23:56:51 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

--- src/baek/baek_3003.java
+++ src/baek/baek_3003.java
@@ -9,8 +9,9 @@ public class baek_3003 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] cnt = new int[6];
         int[] full = {1, 1, 2, 2, 2, 8};
+        String token[] = br.readLine().split(" ");
         for(int i =0; i<6;i++){
-            cnt[i] = Integer.parseInt(br.readLine());
+            cnt[i] = Integer.parseInt(token[i]);
         }
         for(int i =0; i<full.length; i++){
             cnt[i] = full[i] - cnt[i];
