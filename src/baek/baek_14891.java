5891456febae729a80de75171758143569681368
yonghyeon <qodydgus97@naver.com>
2021-11-30 17:02:58 +0900

    톱니바퀴

---
+++ src/baek/baek_14891.java
@@ -0,0 +1,19 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class baek_14891 {
+    public static ArrayList<Integer> gear;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //톱니 4개 입력
+        for (int i =0; i<4; i++){
+            String token[] = br.readLine();
+
+        }
+    }
+}
