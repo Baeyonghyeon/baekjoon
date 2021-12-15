63109ba4d0af302867c9d9b05952a1acf728e709
Baeyonghyeon <qodydgus97@naver.com>
2021-12-15 23:46:56 +0900

    test

--- src/baek/testclass.java
+++ src/baek/testclass.java
@@ -1,10 +1,22 @@
 package baek;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class testclass {
     public static void main(String[] args) throws IOException {
-        System.out.println("파이팅!!");
-        System.out.println("파이팅!!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = 10;
+        double b = 10.1;
+
+        if(a>b){
+            System.out.println("a가 더 크다");
+        }else{
+            System.out.println("b가더 크다");
+        }
+
     }
+
 }
