e3db3314b591c6271b1efe3863790a5ca0530f15
yonghyeon <qodydgus97@naver.com>
2021-12-22 01:30:27 +0900

    최소공배수

--- src/baek/baek_13241.java
+++ src/baek/baek_13241.java
@@ -4,23 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class baek_1934 {
+public class baek_13241 {
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            String token[] = br.readLine().split(" ");
-            int x = Integer.parseInt(token[0]);
-            int y = Integer.parseInt(token[1]);
-            sb.append(lcm(x, y)).append("\n");
-        }
+        String token[] = br.readLine().split(" ");
+        long x = Long.parseLong(token[0]);
+        long y = Long.parseLong(token[1]);
+        sb.append(lcm(x, y));
         System.out.println(sb);
     }
 
     //최대공약수 (유클리드 호제법)  x<y 가정
-    public static int gcd(int x, int y) {
+    public static long gcd(long x, long y) {
         if (y == 0) {
             return x;
         }
@@ -28,7 +25,7 @@ public class baek_1934 {
     }
 
     //최소공배수 (0제외한 두 수의곱 / 최대공약수)
-    public static int lcm(int x, int y) {
+    public static long lcm(long x, long y) {
         return (x * y) / gcd(x, y);
     }
 }
