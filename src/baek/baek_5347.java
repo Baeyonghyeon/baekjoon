66074e2df6221e3a3b049599d085780ff219984b
yonghyeon <qodydgus97@naver.com>
2021-12-22 01:48:08 +0900

    LCM

---
+++ src/baek/baek_5347.java
@@ -0,0 +1,35 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baek_5347 {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String token[] = br.readLine().split(" ");
+            long x = Long.parseLong(token[0]);
+            long y = Long.parseLong(token[1]);
+            sb.append(lcm(x, y)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    //최대공약수 (유클리드 호제법)  x<y 가정
+    public static long gcd(long x, long y) {
+        if (y == 0) {
+            return x;
+        }
+        return gcd(y, x % y);
+    }
+
+    //최소공배수 (0제외한 두 수의곱 / 최대공약수)
+    public static long lcm(long x, long y) {
+        return (x * y) / gcd(x, y);
+    }
+
+}
