5fad6a06aa61a472174ca552761d88727ea263ae
Baeyonghyeon <qodydgus97@naver.com>
2021-12-28 18:12:46 +0900

    소수 찾기

--- src/baek/baek_1978.java
+++ src/baek/baek_1978.java
@@ -1,25 +1,34 @@
 package baek;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class baek_1978 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int n = scan.nextInt();
-        int cnt=0;
-
-        for (int i=0; i<n; i++){
-            int input = scan.nextInt();
-            if(input == 1 ){
-                continue;
+    public static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int list[] = new int[N];
+        String token[] = br.readLine().split(" ");
+        for(int i=0; i<N; i++){
+            Boolean bl = true;
+            list[i] = Integer.parseInt(token[i]);
+            if(list[i]== 1){
+                bl = false;
             }
-            for(int j=2; j<=Math.sqrt(input);j++){
-                if(input%j == 0) continue;
+            for(int j=2; j<=Math.sqrt(list[i]); j++){
+                if(list[i] % j == 0){
+                    bl = false;
+                    break;
+                }
             }
 
-            cnt++;
+            if(bl == true){
+                count++;
+            }
         }
-        System.out.println(cnt);
+
+        System.out.println(count);
     }
 }
