6e937f71107a388f4295ddc54a9487cd26a458ca
Yonghyeon Bae <qodydgus97@naver.com>
2022-01-08 17:04:13 +0900

    피보나치 수 5

--- src/baek/baek_10870.java
+++ src/baek/baek_10870.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class baek_10870 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int N = scan.nextInt();
 
         System.out.println(fibo(N));
