41a959ef8a2e967b9684feb2dc1605d61ac275ce
Baeyonghyeon <qodydgus97@naver.com>
2022-01-11 23:51:08 +0900

    최대 힙

---
+++ src/baek/baek_11279.java
@@ -0,0 +1,28 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class baek_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>(((o1, o2) -> o2 - o1));
+
+        while (N-->0){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){
+                if(q.isEmpty()){
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(q.poll()).append("\n");
+                }
+            } else {
+                q.offer(n);
+            }
+        }
+    }
+}
