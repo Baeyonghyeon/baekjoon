54308a5e6abffb9094bd532e664df543b158706a
yonghyeon <qodydgus97@naver.com>
2022-02-05 23:41:43 +0900

    첫 글자를 대문자로

---
+++ src/baek/baek_4458.java
@@ -0,0 +1,17 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baek_4458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            System.out.println(str.substring(0,1).toUpperCase()+str.substring(1));
+        }
+    }
+}
