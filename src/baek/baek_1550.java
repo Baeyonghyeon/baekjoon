d7ef9731de23c4ff266ca2c4361b5cf8dc5cb28c
yonghyeon <qodydgus97@naver.com>
2022-02-08 22:42:52 +0900

    뒤집힌 덧셈

--- src/baek/baek_1550.java
+++ src/baek/baek_1550.java
@@ -8,6 +8,7 @@ public class baek_1550 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine(),16); // 16진수로 표현
+
         System.out.println(N);
     }
 }
