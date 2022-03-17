46b314d4ba58976adcd1285c698d400fe1e52e92
Baeyonghyeon <qodydgus97@naver.com>
2022-03-18 00:25:25 +0900

    자바공부 9일차

--- src/javaStudy/day9/DeadLockSample.java
+++ src/javaStudy/day9/DeadLockSample.java
@@ -5,38 +5,41 @@ public class DeadLockSample {
     public static Object memoryB = new Object();
 
     public static void main(String args[]) {
-        ThreadDemo1 T1 = new ThreadDemo1();
-        ThreadDemo2 T2 = new ThreadDemo2();
-        T1.start();
-        T2.start();
+        Thread2 t2 = new Thread2();
+        Thread3 t3 = new Thread3();
+        t2.start();
+        t3.start();
     }
 
-    private static class ThreadDemo1 extends Thread {
+    private static class Thread2 extends Thread {
         public void run() {
             synchronized (memoryA) {
-                System.out.println("Thread 1: Holding lock 1...");
+                System.out.println("Thread2는 공유 자원(A) 점유(사용)하고 있습니다.");
 
-                try { Thread.sleep(10); }
+                try { Thread.sleep(100); }
                 catch (InterruptedException e) {}
-                System.out.println("Thread 1: Waiting for lock 2...");
+
+                System.out.println("Thread2는 공유자원(B) 사용 대기중입니다. : " + this.getState());
 
                 synchronized (memoryB) {
-                    System.out.println("Thread 1: Holding lock 1 & 2...");
+                    System.out.println("작업을 못해요");
                 }
             }
         }
     }
-    private static class ThreadDemo2 extends Thread {
+
+    private static class Thread3 extends Thread {
         public void run() {
             synchronized (memoryB) {
-                System.out.println("Thread 2: Holding lock 2...");
+                System.out.println("Thread3는 공유 자원(B) 점유(사용)하고 있습니다.");
 
-                try { Thread.sleep(10); }
+                try { Thread.sleep(100); }
                 catch (InterruptedException e) {}
-                System.out.println("Thread 2: Waiting for lock 1...");
+
+                System.out.println("Thread3는 공유자원(A) 사용 대기중입니다. : "+ this.getState());
 
                 synchronized (memoryA) {
-                    System.out.println("Thread 2: Holding lock 1 & 2...");
+                    System.out.println("작업을 못해요");
                 }
             }
         }
