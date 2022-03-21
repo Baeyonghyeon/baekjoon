c293a16e691edafc2d505e02e4f49611a3371a50
Baeyonghyeon <qodydgus97@naver.com>
2022-03-21 21:47:38 +0900

    드래곤 게임 과제

---
+++ src/javaStudy/day11/server/MyChatClient.java
@@ -0,0 +1,112 @@
+package javaStudy.day11.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MyChatClient {
+    private final String id;
+
+    public MyChatClient(String id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        if (hasNotArgs(args)) {
+            System.out.println("USAGE: java MyChatClient {id}");
+            return;
+        }
+        String id = args[0];
+        MyChatClient client = new MyChatClient(id);
+        client.connect("127.0.0.1", 8888);
+    }
+
+    private static boolean hasNotArgs(String[] args) {
+        return args.length == 0;
+    }
+
+    private void connect(String serverHost, int port) {
+        try {
+            Socket socket = new Socket(serverHost, port);
+            System.out.println("Connected to server " + serverHost + ":" + port);
+            Thread sender = new Sender(socket, id); // 메시지 보내는 스레드
+            Thread receiver = new Receiver(socket); // 받는 스레드
+
+            sender.start();
+            receiver.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Sender extends Thread {
+        private String id;
+        private DataOutputStream out;
+
+        private Sender(Socket socket, String id) throws IOException {
+            this.id = id;
+            this.out = new DataOutputStream(socket.getOutputStream());
+        }
+
+        @Override
+        public void run() {
+            try {
+                initialize();
+                sendMessage();
+            } catch (IOException e) {
+                // TODO
+            }
+        }
+
+        private void initialize() throws IOException {
+            //아이디를 서버에 보내면서 시작
+            if (isSendable()) {
+                this.out.writeUTF(id);
+            }
+        }
+
+        private boolean isSendable() {
+            return this.out != null;
+        }
+
+        private void sendMessage() throws IOException {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (isSendable()) {
+                    this.out.writeUTF(scanner.nextLine());
+                }
+            }
+        }
+    }
+
+    private static class Receiver extends Thread {
+        private final DataInputStream in;
+
+        private Receiver(Socket socket) throws IOException {
+            this.in = new DataInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void run() {
+            while (isReceivable()) {
+                receiveMessage();
+            }
+        }
+
+        private boolean isReceivable() {
+            return this.in != null;
+        }
+
+        private void receiveMessage() {
+            try {
+                System.out.println(in.readUTF());
+            } catch (IOException e) {
+                // TODO
+            }
+        }
+    }
+}
