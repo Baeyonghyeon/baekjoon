0412c789981db7896762115bb6cb280a89b5917b
kurt <qodydgus97@kakao.com>
2026-03-17 21:07:20 +0900

    백준 - "가장 큰 증가하는 부분 수열"

---
+++ src/baek/baek_11055.java
@@ -0,0 +1,36 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baek_11055 {
+
+    static int[] A;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = new int[N];
+        dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = A[i];
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j])
+                    dp[i] = Math.max(dp[i], A[i] + dp[j]);
+            }
+        }
+
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
