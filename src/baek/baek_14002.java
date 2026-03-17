773244fb05f32f26cf740c3f0952edd31abc56d4
kurt <qodydgus97@kakao.com>
2026-03-17 21:07:24 +0900

    백준 - "가장 긴 증가하는 부분 수열 4"

--- src/baek/baek_14002.java
+++ src/baek/baek_14002.java
@@ -1,55 +1,61 @@
 package baek;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
 public class baek_14002 {
 
-    static List<Integer> dp = new ArrayList<>();
-    static List<Integer> result = new ArrayList<>();
+    static int[] arr;
+    static int[] dp;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        dp.add(Integer.valueOf(st.nextToken()));
-        result.add(dp.get(0));
 
-        for (int i = 1; i < N; i++) {
-            int value = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dp = new int[n];
+        parent = new int[n];
+        Arrays.fill(parent, -1);
 
-            if (dp.get(dp.size() - 1) < value) {
-                dp.add(value);
-                result.add(value);
-            } else if (dp.get(dp.size() - 1) > value) {
-                int index = binarySearch(dp, 0, dp.size(), value);
-                dp.set(index, value);
-                if (result.get(index) > value) {
-                    result.set(index, value);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
                 }
             }
         }
 
-        System.out.println(dp.size());
-        String output = result.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(" "));
-        System.out.print(output);
-
-    }
-
-    private static int binarySearch(List<Integer> arr, int start, int end, int value) {
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (arr.get(mid) < value) start = mid + 1;
-            else end = mid;
+        int maxLen = 0;
+        int maxIdx = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIdx = i;
+            }
         }
 
-        return end;
+        List<Integer> result = new ArrayList<>();
+        int idx = maxIdx;
+        while (idx != -1) {
+            result.add(arr[idx]);
+            idx = parent[idx];
+        }
+        Collections.reverse(result);
+
+        System.out.println(maxLen);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
