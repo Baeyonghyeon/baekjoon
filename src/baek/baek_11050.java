d3ad3357eb78465043785dd696e34b478e37ff60
kurt <qodydgus97@kakao.com>
2026-03-17 21:07:19 +0900

    백준 - "이항 계수 1"

--- src/baek/baek_11050.java
+++ src/baek/baek_11050.java
@@ -3,20 +3,44 @@ package baek;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class baek_11050 {
-    public static void main(String[] args) throws IOException {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String token[] = br.readLine().split(" ");
+//        int N = Integer.parseInt(token[0]);
+//        int K = Integer.parseInt(token[1]);
+//
+//        System.out.println(factorial(N)/(factorial(N-K)*factorial(K)));
+//    }
+//
+//    static int factorial(int N) {
+//        if (N <= 1) return 1;
+//
+//        return N * factorial(N - 1);
+//    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String token[] = br.readLine().split(" ");
-        int N = Integer.parseInt(token[0]);
-        int K = Integer.parseInt(token[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(factorial(N)/(factorial(N-K)*factorial(K)));
-    }
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] D = new int[n + 1][n + 1];
 
-    static int factorial(int N) {
-        if (N <= 1) return 1;
+        for (int i = 1; i <= n; i++) {
+            D[i][0] = 1;
+            D[i][1] = i;
+            D[i][i] = 1;
+        }
 
-        return N * factorial(N - 1);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j - 1] + D[i - 1][j];
+            }
+        }
+
+        System.out.println(D[n][k]);
     }
 }
