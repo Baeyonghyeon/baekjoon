aa694be69d8eabadaf4ba248ae58ab5c8a35227e
kurt <qodydgus97@kakao.com>
2026-03-17 21:07:16 +0900

    백준 - "동전 1"

---
+++ src/baek/baek_2293.java
@@ -0,0 +1,32 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baek_2293 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int sum = coin; sum <= k; sum++) {
+                dp[sum] += dp[sum - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
